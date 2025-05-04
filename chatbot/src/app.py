c6798593c52a5c3523266836182d11020c43e892
Lee Roland <glroland@hotmail.com>
2025-05-04 08:17:05 -0400

    made the agent urls configurable (duh)

--- chatbot/src/app.py
+++ chatbot/src/app.py
@@ -2,6 +2,7 @@
 
 Digital expert in all things MLB.
 """
+import os
 import logging
 import streamlit as st
 from constants import SessionStateVariables
@@ -10,6 +11,9 @@ from constants import CannedGreetings
 from constants import MessageAttributes
 from constants import AGENT_SYSTEM_PROMPT
 from lls_gateway import lls_connect, lls_create_agent, lls_new_session
+
+ENV_AGENT_WEATHER_URL = "AGENT_WEATHER_URL"
+ENV_AGENT_TEAM_URL = "AGENT_TEAM_URL"
 
 logger = logging.getLogger(__name__)
 
@@ -23,18 +27,32 @@ logging.basicConfig(level=logging.DEBUG,
 if SessionStateVariables.MESSAGES not in st.session_state:
     st.session_state[SessionStateVariables.MESSAGES] = []
 
+    # Get configurable values
+    agent_team_url = os.environ[ENV_AGENT_TEAM_URL]
+    if agent_team_url is None or len(agent_team_url):
+        msg = "Agent Team URL is required and cannot be empty!"
+        logger.error(msg)
+        raise ValueError(msg)
+    logger.info("Team Agent URL: %s", agent_team_url)
+    agent_weather_url = os.environ[ENV_AGENT_WEATHER_URL]
+    if agent_weather_url is None or len(agent_weather_url):
+        msg = "Agent Weather URL is required and cannot be empty!"
+        logger.error(msg)
+        raise ValueError(msg)
+    logger.info("Weather Agent URL: %s", agent_weather_url)
+
     # Connect to LLama Stack
     logger.info("Initializing LLama Stack")
     llama_stack_client, llama_stack_model = lls_connect()
     llama_stack_client.toolgroups.register(
         toolgroup_id="agent-team",
         provider_id="model-context-protocol",
-        mcp_endpoint={"uri": "https://b1-agent-team-baseball-chatbot.apps.ocp.home.glroland.com/sse"},
+        mcp_endpoint={"uri": agent_team_url},
     )
     llama_stack_client.toolgroups.register(
         toolgroup_id="agent-weather",
         provider_id="model-context-protocol",
-        mcp_endpoint={"uri": "https://b1-agent-weather-baseball-chatbot.apps.ocp.home.glroland.com/sse"},
+        mcp_endpoint={"uri": agent_weather_url},
     )
     llama_stack_agent = lls_create_agent(llama_stack_client, AGENT_SYSTEM_PROMPT, ["agent-team", "agent-weather"])
     logger.info("LLama Stack Initialized")
